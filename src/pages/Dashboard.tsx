c464daa8f5313c5d6b3510d0a72d6739fa72e7ae
youfoundmalik <godswillugochukwu88@gmail.com>
2023-09-05 17:59:02 +0100

    success modal

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,16 +7,15 @@ const Dashboard = () => {
   const { user } = useContext(StoreContext);
   return (
     <section className='w-screen h-screen flex flex-col items-center justify-center'>
-      <div className='border border-black border-opacity-20 px-2 py-10 md:px-5 rounded-md w-full m-3 md:w-2/3 text-center lg:w-1/2'>
-        <div className='w-[70%] mb-5'>
+      <div className='border border-black bg-[#f7f7f7] border-opacity-20 px-2 py-10 md:px-5 rounded-md w-[90%] m-3 md:w-2/3 text-center lg:w-1/2'>
+        <div className='w-[70%] md:w-[50%] mx-auto md:mb-3 mb-2'>
           <img alt='authenticate' src={success} />
         </div>
-        <p className='text-base'>
-          Login to{' '}
-          <span className='inline-block text-lg font-bold text-teal-700'>
-            {user?.email}
-          </span>{' '}
-          successful
+        <p className='text-base md:text-[18px] font-[500] mb-1'>
+          Login successful
+        </p>
+        <p className='inline-block text-lg md:text-[20px] font-bold text-teal-700'>
+          {user?.email}
         </p>
       </div>
     </section>
